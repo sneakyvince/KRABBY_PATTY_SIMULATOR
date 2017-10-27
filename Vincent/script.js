29b40c323d852e314c3168d46207d9f37e4f4ac0
Vincent van der Palen <vincent@MacBook-Pro-van-Vincent.local>
2017-10-27 09:39:57 +0200

    adds burger visual for voice commands
    Burger will build as you call out the correct ingredients

--- Vincent/script.js
+++ Vincent/script.js
@@ -2,7 +2,7 @@ try {
   var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   var recognition = new SpeechRecognition();
 }
-catch(e) {
+catch (e) {
   console.error(e);
   $('.no-browser-support').show();
   $('.app').hide();
@@ -11,10 +11,7 @@ catch(e) {
 
 var noteTextarea = $('#note-textarea');
 var instructions = $('#recording-instructions');
-var notesList = $('ul#notes');
-
-var noteContent = '';
-//var wordArray = ["broodje", "kaas", "hamburger"]
+var krabbyArray = ["bread", "sesame", "hamburger", "lettuce", "cheese",  "onion"];
 
 
 /*-----------------------------
@@ -26,9 +23,10 @@ var noteContent = '';
 // allowing us to keep recording even when the user pauses. 
 recognition.continuous = true;
 recognition.interimResults = true;
+recognition.lang = "en-GB";
 
 // This block is called every time the Speech APi captures a line. 
-recognition.onresult = function(event) {
+recognition.onresult = function (event) {
 
   // event is a SpeechRecognitionEvent object.
   // It holds all the lines we have captured so far. 
@@ -43,23 +41,19 @@ recognition.onresult = function(event) {
   // There is no official solution so far so we have to handle an edge case.
   var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
 
-  if(!mobileRepeatBug) {
-    noteContent += transcript;
-    noteTextarea.val(noteContent);
-  }
 };
 
-recognition.onstart = function() { 
+recognition.onstart = function () {
   instructions.text('Voice recognition activated. Try speaking into the microphone.');
 }
 
-recognition.onspeechend = function() {
+recognition.onspeechend = function () {
   instructions.text('You were quiet for a while so voice recognition turned itself off.');
 }
 
-recognition.onerror = function(event) {
-  if(event.error == 'no-speech') {
-    instructions.text('No speech was detected. Try again.');  
+recognition.onerror = function (event) {
+  if (event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.');
   };
 }
 
@@ -68,28 +62,47 @@ recognition.onerror = function(event) {
       App buttons and input 
 ------------------------------*/
 
-$('#start-record-btn').on('click', function(e, transcript) {
-  if (noteContent.length) {
-    noteContent += ' ';
-  }
-  recognition.onresult = function(transcript) { 
+$('#start-record-btn').on('click', function (e, transcript) {
+  recognition.onresult = function (transcript) {
 
     var current = event.resultIndex;
     var transcript = event.results[current][0].transcript;
     console.log(transcript);
+
+    if (transcript.indexOf(krabbyArray[0]) >= 0) {
+      document.getElementById("bread").style.visibility = "visible";
+    }
+
+    if (transcript.indexOf(krabbyArray[1]) >= 0) {
+      document.getElementById("sesame").style.visibility = "visible";
+    }
+
+    if (transcript.indexOf(krabbyArray[2]) >= 0) {
+      document.getElementById("meat").style.visibility = "visible";
+    }
+
+    if (transcript.indexOf(krabbyArray[3]) >= 0) {
+      document.getElementById("lettuce").style.visibility = "visible";
+    }
+
+    if (transcript.indexOf(krabbyArray[4]) >= 0) {
+      document.getElementById("cheese").style.visibility = "visible";
+      document.getElementById("melting-cheese").style.visibility = "visible";
+    }
+
+    if (transcript.indexOf(krabbyArray[5]) >= 0) {
+      document.getElementById("onion").style.visibility = "visible";
+    }
+
   }
-  
+
   recognition.start();
 });
 
 
-$('#pause-record-btn').on('click', function(e) {
+$('#pause-record-btn').on('click', function (e) {
   recognition.stop();
   instructions.text('Voice recognition paused.');
 });
 
-// Sync the text inside the text area with the noteContent variable.
-noteTextarea.on('input', function() {
-  noteContent = $(this).val();
-})
 
